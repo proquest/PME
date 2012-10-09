42b30af8d79f53ead4916caab7e4e83f0e83a814
Arthur Langereis <arthur@logic-dream.com>
2012-10-09 20:03:06 +0200

    initial full-cycle extraction of ft.com

---
+++ PME.js
@@ -0,0 +1,464 @@
+(function() {
+"use strict";
+// PME.js
+
+var urlMatchers = {
+	"^https?://(www|search)\\.ft\\.com": "Financial Times"
+};
+
+var baseURL = "http://logic-dream.com/PME/",
+	extractorsBaseURL = baseURL + "extractors/";
+
+var pageURL, pageDoc,
+	injScript;
+
+function log() {
+	var stuff = Array.prototype.slice.call(arguments, 0);
+	stuff.unshift("PME");
+	console.info.apply(console, stuff);
+}
+
+// ------------------------------------------------------------------------
+//                                     _   _ _ 
+//   __ _ _ __ _ __ __ _ _   _   _   _| |_(_) |
+//  / _` | '__| '__/ _` | | | | | | | | __| | |
+// | (_| | |  | | | (_| | |_| | | |_| | |_| | |
+//  \__,_|_|  |_|  \__,_|\__, |  \__,_|\__|_|_|
+//                       |___/                 
+// ------------------------------------------------------------------------
+function mutate(vals, pred) {
+	var arr = vals instanceof Array,
+		out = arr ? [] : {};
+
+	if (arr) {
+		for (var ix = 0; ix < vals.length; ++ix) {
+			if (! ix in vals)
+				continue;
+			var val = vals[ix],
+				subst = pred(val, ix, vals);
+			if (undefined !== subst)
+				out.push(subst);
+		}
+	}
+	else {
+		for (var key in vals) {
+			var val = vals[key],
+				subst = pred(val, key, vals);
+			if (undefined !== subst)
+				out[key] = subst;
+		}
+	}
+
+	return out;
+}
+
+function filter(vals, pred) {
+	var arr = vals instanceof Array,
+		out = arr ? [] : {};
+
+	if (arr) {
+		for (var ix = 0; ix < vals.length; ++ix) {
+			if (! ix in vals)
+				continue;
+			var val = vals[ix];
+			if (true === pred(val, ix, vals))
+				out.push(val);
+		}
+	}
+	else {
+		for (var key in vals) {
+			var val = vals[key];
+			if (true === pred(val, key, vals))
+				out[key] = val;
+		}
+	}
+	return out;
+}
+
+function flatten(vals) {
+	if (! (vals instanceof Array))
+		return vals;
+
+	var out = [];
+	for (var ix = 0; ix < vals.length; ++ix) {
+		var v = vals[ix];
+		if(v instanceof Array)
+			out = out.concat(flatten(v));
+		else
+			out.push(v);
+	}
+	return out;
+}
+
+function purge(vals) {
+	return filter(vals, function(val, key) {
+		return (val !== null) && (val !== undefined);
+	});
+}
+
+
+// ------------------------------------------------------------------------
+//   __ _ _ _                
+//  / _(_) | |_ ___ _ __ ___ 
+// | |_| | | __/ _ \ '__/ __|
+// |  _| | | ||  __/ |  \__ \
+// |_| |_|_|\__\___|_|  |___/
+//                           
+// ------------------------------------------------------------------------
+function ValueFilter() {
+	var filters = [], st;
+
+	function addFilter(fn) {
+		filters.push(fn);
+		return st;
+	}
+
+	function _applyFilters(vals, context) {
+		for (var fi=0; fi<filters.length; ++fi) {
+			vals = purge(flatten(vals));
+
+			for (var vi=0; vi<vals.length; ++vi) {
+				try {
+					vals[vi] = filters[fi](vals[vi], context);
+				}
+				catch(ex) {	}
+			}
+
+			vals = purge(vals);
+		}
+
+		return flatten(vals);
+	}
+
+	st = {
+		addFilter: addFilter,
+		_applyFilters: _applyFilters,
+
+		trim: function() { return addFilter(
+			function(str) {
+				return str.replace(/^\s+|\s+$/g, '');
+			}
+		);},
+		trimInternal: function() { return addFilter(
+			function(str) {
+				return str.replace(/\s+/g, ' ');
+			}
+		);},
+		replace: function(find, subst) { return addFilter(
+			function(str) {
+				return str.replace(find, subst);
+			}
+		);},
+		unescape: function() { return addFilter(
+			function(str) {
+				return unescape(str);
+			}
+		);},
+		unescapeHTML: function() { return addFilter(
+			function(str) {
+				return str; // TBI
+			}
+		);},
+		cleanAuthor: function(type, isReversed) { return addFilter(
+			function(str) {
+				return str; // TBI
+			}
+		);},
+		match: function(against, matchIndex) { return addFilter(
+			function(str) {
+				var m = str.match(against);
+				if (m)
+					m = m[matchIndex || 0];
+				return m;
+			}
+		);},
+		prepend: function(prefix) { return addFilter(
+			function(str) {
+				return prefix + str;
+			}
+		);},
+		append: function(suffix) { return addFilter(
+			function(str) {
+				return str + suffix;
+			}
+		);},
+		key: function(keyName) { return addFilter(
+			function(obj) {
+				return obj[keyName];
+			}
+		);},
+		split: function(sep) { return addFilter(
+			function(str) {
+				return str.split(sep);
+			}
+		);},
+		capitalizeTitle: function() { return addFilter(
+			function(str) {
+				return str; // TBI
+			}
+		);}
+	};
+	return st;
+}
+
+
+function PageText() {
+	var pt = ValueFilter();
+
+	pt.evaluate = function(doc, url) {
+		var vals = url._applyFilters([doc.documentElement.innerHTML], doc);
+		return vals.length ? vals : false;
+	};
+
+	return pt;
+}
+
+function Url() {
+	var lk = ValueFilter();
+
+	lk.evaluate = function(doc, url) {
+		var vals = lk._applyFilters([url], doc);
+		return vals.length ? vals : false;
+	}
+
+	return lk;
+}
+
+function Xpath(selector) {
+	var xp = ValueFilter();
+	xp.text = function() { return xp.addFilter(
+		function(obj) {
+			return obj.textContent || obj;
+		}
+	);};
+
+	xp.evaluate = function(doc, url) {
+		var res  = doc.evaluate(selector, doc, null, XPathResult.ANY_TYPE, null),
+			type = res.resultType,
+			vals = [],
+			v;
+
+		if (type == XPathResult.STRING_TYPE)
+			vals.push(res.stringValue);
+		else {
+			if (type == XPathResult.ORDERED_NODE_ITERATOR_TYPE || type == XPathResult.UNORDERED_NODE_ITERATOR_TYPE) {
+				while(v = res.iterateNext())
+					vals.push(v);
+			}
+		}
+
+		vals = xp._applyFilters(vals, doc);
+		return vals.length ? vals : false;
+	};
+
+	return xp;
+}
+
+
+// ------------------------------------------------------------------------
+//                                         
+//  ___  ___ _ __ __ _ _ __   ___ _ __ ___ 
+// / __|/ __| '__/ _` | '_ \ / _ \ '__/ __|
+// \__ \ (__| | | (_| | |_) |  __/ |  \__ \
+// |___/\___|_|  \__,_| .__/ \___|_|  |___/
+//                    |_|                  
+// ------------------------------------------------------------------------
+window.FW = (function(){
+	var scrapers = [];
+
+	function ScraperBase(spec) {
+		function evalItem(item, doc, url) {
+			if(typeof item == "object") {
+				if(item instanceof Array) {
+					return flatten(
+						mutate(item, function(subItem) {
+							return evalItem(subItem, doc, url);
+						})
+					);
+				}
+				return item.evaluate(doc, url);
+			}
+
+			if (typeof item == "function")
+				return item(doc, url);
+
+			return item;
+		}
+
+		return { spec: spec, evalItem: evalItem };
+	}
+
+	function Scraper(spec) {
+		var scrp = ScraperBase(spec);
+
+		scrp.run = function(doc, url, itemCallback, completionCallback) {
+			var skipList  = { detect:1, itemType:1, attachments:1 },
+				multiList = { creators: 1, tags: 1 },
+
+				item = mutate(spec, function(val, key) {
+					if (key in skipList)
+						return undefined;
+
+					var procVal = scrp.evalItem(val, doc, url);
+					log("Scraper got kv", key, procVal);
+
+					if (key in multiList)
+						return flatten([procVal]);
+
+					return (procVal instanceof Array) ? procVal[0] : procVal;
+				});
+
+			item.itemType = spec.itemType;
+			itemCallback(item);
+			completionCallback();
+		};
+
+		scrapers.push(scrp);
+		return scrp;
+	}
+
+	function MultiScraper(spec) {
+		var scrp = ScraperBase(spec);
+
+		scrp.run = function(doc, url, itemCallback, completionCallback) {
+			// TBI
+			completionCallback();
+		};
+
+		scrapers.push(scrp);
+		return scrp;
+	}
+
+	function detectWeb(doc, url) {
+		var eligible = filter(scrapers, function(sc) {
+			return !!sc.evalItem(sc.spec.detect, doc, url);
+		});
+
+		log("FW.detectWeb eligible scrapers", eligible);
+
+		return eligible.length > 0;
+	}
+
+	function doWeb(doc, url) {
+		log("FW.doWeb called");
+
+		var scraper = filter(scrapers, function(sc) {
+			return !!sc.evalItem(sc.spec.detect, doc, url);
+		})[0];
+
+		var data = { items: [] };
+
+		log("FW.doWeb using scraper", scraper);
+
+		scraper.run(doc, url,
+			function itemDone(item) {
+				data.items.push(item);
+			},
+			function allDone() {
+				// <-- send off data to somewhere
+				log("READY.", data);
+				completed();
+			}
+		);
+	}
+
+
+	return {
+		Scraper: Scraper,
+		MultiScraper: MultiScraper,
+		PageText: PageText,
+		Xpath: Xpath,
+		Url: Url,
+		
+		detectWeb: detectWeb,
+		doWeb: doWeb
+	};
+}());
+
+
+// ------------------------------------------------------------------------
+//            _                  _                 
+//   _____  _| |_ _ __ __ _  ___| |_ ___  _ __ ___ 
+//  / _ \ \/ / __| '__/ _` |/ __| __/ _ \| '__/ __|
+// |  __/>  <| |_| | | (_| | (__| || (_) | |  \__ \
+//  \___/_/\_\\__|_|  \__,_|\___|\__\___/|_|  |___/
+//                                                 
+// ------------------------------------------------------------------------
+function loadExtractor(name) {
+	var scr = document.createElement("script");
+	scr.src = extractorsBaseURL + name + ".js";
+	injScript = scr;
+	scr.onerror = function() { log("extractor FAILED: ", name); }
+	document.getElementsByTagName("head")[0].appendChild(scr);
+}
+
+function extractorLoaded(spec, api) {
+	log("extractorLoaded called with spec", spec);
+	if (api.detectWeb(pageDoc, pageURL))
+		api.doWeb(pageDoc, pageURL);
+	else
+		completed();
+}
+
+function exporterNameForURL(url) {
+	var name = null;
+
+	for (var re in urlMatchers) {
+		if (name) break;
+		if (new RegExp(re).test(url))
+			name = urlMatchers[re];
+	}
+
+	return name;
+}
+
+
+// ------------------------------------------------------------------------
+//                   _   _                
+//  _ __ _   _ _ __ | |_(_)_ __ ___   ___ 
+// | '__| | | | '_ \| __| | '_ ` _ \ / _ \
+// | |  | |_| | | | | |_| | | | | | |  __/
+// |_|   \__,_|_| |_|\__|_|_| |_| |_|\___|
+//                                        
+// ------------------------------------------------------------------------
+function vanish() {
+	return; // TSR
+
+	delete window.PME;
+	delete window.FW;
+
+	if (injScript)
+		injScript.parentNode.removeChild(injScript);
+	injScript = null;
+}
+
+function completed() {
+	log("completed");
+	setTimeout(vanish, 1);
+}
+
+function init() {
+	pageURL = document.location.href;
+	pageDoc = document;
+
+	var expName = exporterNameForURL(pageURL);
+
+	if (! expName) {
+		alert("No dice.");
+		completed();
+		return;
+	}
+
+	loadExtractor(expName);
+}
+
+// -- expose extractor APIs
+window.PME = {
+	loadExtractor: loadExtractor,
+	extractorLoaded: extractorLoaded
+};
+
+// -- begin
+init();
+
+}());
---
+++ extractors/wsj.js
@@ -0,0 +1,231 @@
+{
+	"translatorID": "53f8d182-4edc-4eab-b5a1-141698a1303b",
+	"label": "Wall Street Journal",
+	"creator": "Sebastian Karcher",
+	"target": "^http://(online|blogs)?\\.wsj\\.com/",
+	"minVersion": "2.1",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsbv",
+	"lastUpdated": "2012-04-24 01:05:37"
+}
+
+
+
+/*
+Wall Street Journal Translator
+Copyright (C) 2011 Sebastian Karcher
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+function detectWeb(doc, url) { return FW.detectWeb(doc, url); }
+function doWeb(doc, url) { return FW.doWeb(doc, url); }
+
+
+/**Blogs*/
+FW.Scraper({
+itemType         : 'blogPost',
+detect           : FW.Url().match(/blogs\.wsj\.com/	),
+title            : FW.Xpath('//h1').text().trim(),
+attachments      : [
+  {
+  url: FW.Url(),
+  title: "WSJ-Blogs Snapshot",
+  type: "text/html"}],
+creators         : FW.Xpath('//h3[@class="byline"]').text().replace(/^\s*By\s*/, "").cleanAuthor("author"),
+date             : FW.Xpath('//li[@class="dateStamp first"]').text(),
+ISSN			 : "0099-9660",
+publicationTitle : FW.Xpath('//h5[@class="blogtitle"]').text().prepend("WSJ Blogs - ")
+}); 
+ 
+ 
+/** Articles */
+FW.Scraper({
+itemType         : 'newspaperArticle',
+detect           : FW.Url().match(/wsj\.com\/article/),
+title            : FW.Xpath('//h1').text().trim(),
+attachments      : [
+  {
+  url: FW.Url(),
+  title: "Wall Street Journal Snapshot",
+  type: "text/html"}],
+creators         : FW.Xpath('//h3[@class="byline"]/a | //div[@id="recipeACShopAndBuyText"]/h3[@class="byline"]').text().capitalizeTitle(true).replace(/^\s*By\s*/, "").split(/,| and /).cleanAuthor("author"),
+date             : FW.Xpath('//li[@class="dateStamp"]').text().capitalizeTitle(true),
+abstractNote     : FW.Xpath('//meta[@name="description"]/@content').text(),
+section          : FW.Xpath('//li[@class="articleSection first"]/a').text().capitalizeTitle(true),
+ISSN			 : "0099-9660",
+publicationTitle : "Wall Street Journal",
+});
+ 
+
+ 
+FW.MultiScraper({
+itemType         : 'multiple',
+detect           : FW.Url().match(/wsj\.com\/search\//),
+choices          : {
+  titles :  FW.Xpath('//h2/a').text().trim(),
+  urls    :  FW.Xpath('//h2/a').key("href")
+}
+});
+
+
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://online.wsj.com/article/SB10001424052970204517204577046222233016362.html?mod=WSJ_hp_LEFTWhatsNewsCollection",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"creators": [
+					{
+						"firstName": "John W.",
+						"lastName": "Miller",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"url": "http://online.wsj.com/article/SB10001424052970204517204577046222233016362.html?mod=WSJ_hp_LEFTWhatsNewsCollection",
+						"title": "Wall Street Journal Snapshot",
+						"type": "text/html"
+					}
+				],
+				"url": "http://online.wsj.com/article/SB10001424052970204517204577046222233016362.html?mod=WSJ_hp_LEFTWhatsNewsCollection",
+				"abstractNote": "A profile of an Australian miner making $200,000 a year, published in The Wall Street Journal, led hundreds of people to ask how they could apply for such a job.",
+				"date": "November 19, 2011",
+				"ISSN": "0099-9660",
+				"publicationTitle": "Wall Street Journal",
+				"section": "Careers",
+				"title": "America's Jobless, Yearning for Oz",
+				"libraryCatalog": "Wall Street Journal",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://online.wsj.com/article/SB10001424052970203471004577144672783559392.html?mod=WSJ_article_forsub",
+		"items": [
+			{
+				"itemType": "newspaperArticle",
+				"creators": [
+					{
+						"firstName": "Jenny",
+						"lastName": "Strasburg",
+						"creatorType": "author"
+					},
+					{
+						"firstName": "Susan",
+						"lastName": "Pulliam",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"url": "http://online.wsj.com/article/SB10001424052970203471004577144672783559392.html?mod=WSJ_article_forsub",
+						"title": "Wall Street Journal Snapshot",
+						"type": "text/html"
+					}
+				],
+				"url": "http://online.wsj.com/article/SB10001424052970203471004577144672783559392.html?mod=WSJ_article_forsub",
+				"abstractNote": "An outspoken analyst who is embroiled in the Wall Street insider-trading investigation allegedly left threatening messages for two FBI agents.",
+				"date": "January 7, 2012",
+				"ISSN": "0099-9660",
+				"publicationTitle": "Wall Street Journal",
+				"section": "Markets",
+				"title": "An Odd Turn in Insider Case",
+				"libraryCatalog": "Wall Street Journal",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://blogs.wsj.com/overheard/2012/01/06/the-ego-has-landed/",
+		"items": [
+			{
+				"itemType": "blogPost",
+				"creators": [],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"url": "http://blogs.wsj.com/overheard/2012/01/06/the-ego-has-landed/",
+						"title": "WSJ-Blogs Snapshot",
+						"type": "text/html"
+					}
+				],
+				"url": "http://blogs.wsj.com/overheard/2012/01/06/the-ego-has-landed/",
+				"date": "January 6, 2012, 4:22 PM",
+				"ISSN": "0099-9660",
+				"publicationTitle": "WSJ Blogs - Overheard",
+				"title": "The Ego Has Landed",
+				"libraryCatalog": "Wall Street Journal",
+				"accessDate": "CURRENT_TIMESTAMP"
+			}
+		]
+	},
+	{
+		"type": "web",
+		"url": "http://online.wsj.com/search/term.html?KEYWORDS=argentina&mod=DNH_S",
+		"items": "multiple"
+	},
+	{
+		"type": "web",
+		"url": "http://blogs.wsj.com/economics/2012/01/07/number-of-the-week-americans-cheaper-restaurant-bills/",
+		"items": [
+			{
+				"itemType": "blogPost",
+				"creators": [
+					{
+						"firstName": "Phil",
+						"lastName": "Izzo",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"url": "http://blogs.wsj.com/economics/2012/01/07/number-of-the-week-americans-cheaper-restaurant-bills/",
+						"title": "WSJ-Blogs Snapshot",
+						"type": "text/html"
+					}
+				],
+				"url": "http://blogs.wsj.com/economics/2012/01/07/number-of-the-week-americans-cheaper-restaurant-bills/",
+				"date": "January 7, 2012, 5:00 AM",
+				"ISSN": "0099-9660",
+				"publicationTitle": "WSJ Blogs - Real Time Economics",
+				"title": "Number of the Week: Americans’ Cheaper Restaurant Bills",
+				"libraryCatalog": "Wall Street Journal",
+				"accessDate": "CURRENT_TIMESTAMP",
+				"shortTitle": "Number of the Week"
+			}
+		]
+	}
+]
+/** END TEST CASES **/
