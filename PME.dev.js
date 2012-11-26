c4f99942d8affd756b16aa63fceba7dd80114312
Arthur Langereis <arthur@logic-dream.com>
2012-11-26 16:17:47 +0100

    pubmed and temp push

---
+++ PME.dev.js
@@ -0,0 +1,688 @@
+(function() {
+"use strict";
+// PME.js
+
+var urlMatchers = {
+	"^https?://(www|search)\\.ft\\.com": "Financial Times",
+	"^http://(online|blogs)?\\.wsj\\.com/": "wsj",
+	"https?://[^/]*.nih.gov/": "PubMed Central"
+};
+
+var extractorsBaseURL = 'http://' + PME_SRV + "/extractors/";
+
+var pageURL, pageDoc,
+	pmeCallback,
+	transScript;
+
+function log() {
+	var stuff = Array.prototype.slice.call(arguments, 0);
+	stuff.unshift("PME");
+	if(window.console && console.info) {
+		if (console.info.apply)
+			console.info.apply(console, stuff);
+		else // IE...
+			console.info(stuff.join(", "));
+	}
+}
+
+// ------------------------------------------------------------------------
+//                                     _   _ _ 
+//   __ _ _ __ _ __ __ _ _   _   _   _| |_(_) |
+//  / _` | '__| '__/ _` | | | | | | | | __| | |
+// | (_| | |  | | | (_| | |_| | | |_| | |_| | |
+//  \__,_|_|  |_|  \__,_|\__, |  \__,_|\__|_|_|
+//                       |___/                 
+// ------------------------------------------------------------------------
+function each(vals, handler) {
+	var arr = "length" in vals,
+		out = arr ? [] : {};
+
+	if (arr) {
+		for (var ix = 0; ix < vals.length; ++ix) {
+			if (! ix in vals)
+				continue;
+			handler(vals[ix], ix, vals);
+		}
+	}
+	else {
+		for (var key in vals)
+			pred(vals[key], key, vals);
+	}
+
+	return out;
+}
+
+function map(vals, pred) {
+	var arr = "length" in vals,
+		out = arr ? [] : {};
+
+	if (arr) {
+		for (var ix = 0; ix < vals.length; ++ix) {
+			if (! ix in vals)
+				continue;
+			var val = vals[ix],
+				subst = pred(val, ix, vals);
+			if (undefined !== subst)
+				out.push(subst);
+		}
+	}
+	else {
+		for (var key in vals) {
+			var val = vals[key],
+				subst = pred(val, key, vals);
+			if (undefined !== subst)
+				out[key] = subst;
+		}
+	}
+
+	return out;
+}
+
+function filter(vals, pred) {
+	var arr = "length" in vals,
+		out = arr ? [] : {};
+
+	if (arr) {
+		for (var ix = 0; ix < vals.length; ++ix) {
+			if (! ix in vals)
+				continue;
+			var val = vals[ix];
+			if (true === pred(val, ix, vals))
+				out.push(val);
+		}
+	}
+	else {
+		for (var key in vals) {
+			var val = vals[key];
+			if (true === pred(val, key, vals))
+				out[key] = val;
+		}
+	}
+	return out;
+}
+
+function flatten(vals) {
+	if (! ("length" in vals))
+		return vals;
+
+	var out = [];
+	for (var ix = 0; ix < vals.length; ++ix) {
+		var v = vals[ix];
+		if(v instanceof Array)
+			out = out.concat(flatten(v));
+		else
+			out.push(v);
+	}
+	return out;
+}
+
+function purge(vals) {
+	return filter(vals, function(val, key) {
+		return (val !== null) && (val !== undefined);
+	});
+}
+
+function makeArray(x) {
+	if (x == null)
+		return [];
+
+	return ("length" in x) ? x : [x];
+}
+
+
+// ------------------------------------------------------------------------
+//  ____  __  __ _____                      
+// |  _ \|  \/  | ____|   ___ ___  _ __ ___ 
+// | |_) | |\/| |  _|    / __/ _ \| '__/ _ \
+// |  __/| |  | | |___  | (_| (_) | | |  __/
+// |_|   |_|  |_|_____|  \___\___/|_|  \___|
+//                                          
+// ------------------------------------------------------------------------
+window.PME = {};
+PME.items = [];
+
+PME.wait = function() {};
+PME.done = function() {
+	log("done(), item count: " + PME.items.length);
+	completed(PME.items.length ? { items: PME.items } : null);
+};
+
+PME.debug = function(str) {
+	log("[ext]", str);
+};
+
+PME.selectItems = function(items, callback) {
+	setTimeout(function() {			// use async return in case code relies on it
+		var out = {};
+		for (var k in items) {
+			out[k] = items[k];		// always just pick the first one for now
+			break;
+		}
+
+		callback(out);
+	}, 1);
+};
+
+
+PME.Item = function(type) {
+	log("creating item of type " + type);
+	this.itemType = type;
+	this.creators = [];
+
+	this.complete = function() {
+		log("item completed", this);
+		PME.items.push(this);
+		delete this.complete;
+	};
+};
+
+
+
+// ------------------------------------------------------------------------
+//  _   _ _   _ _ 
+// | | | | |_(_) |
+// | | | | __| | |
+// | |_| | |_| | |
+//  \___/ \__|_|_|
+//                
+// ------------------------------------------------------------------------
+PME.Util = {};
+
+PME.Util.trim = function(str) {
+	return str.replace(/^\s+|\s+$/g, '')
+};
+
+PME.Util.xpath = function(nodes, selector, namespaces) {
+	var out = [];
+
+	each(makeArray(nodes), function(node) {
+		var doc = node.ownerDocument ? node.ownerDocument : (node.documentElement ? node : null);
+
+		function resolver(prefix) { return namespaces && namespaces[prefix]; }
+
+		if ("evaluate" in doc) {
+			var xp = doc.evaluate(selector, node, resolver, XPathResult.ANY_TYPE, null),
+				el;
+
+			while (el = xp.iterateNext())
+				out.push(el);
+		}
+		else if ("selectNodes" in node) {
+			if (namespaces) {
+				var selNS = map(namespaces, function(url, prefix) {
+					return 'xmlns:' + prefix + '="' + url + '"';
+				});
+				doc.setProperty("SelectionNamespaces", selNS.join(" "));
+			}
+
+			var sn = node.selectNodes(selector);
+			for (var i=0; i < sn.length; ++i)
+				out.push(sn[i]);
+		}
+	});
+	
+	return out;
+};
+
+PME.Util.xpathText = function(nodes, selector, namespaces, delim) {
+	nodes = PME.Util.xpath(nodes, selector, namespaces);
+	if (! nodes.length)
+		return null;
+	
+	var text = map(nodes, function(node) {
+		return node.textContent || node.innerText || node.text || node.nodeValue;
+	});
+
+	return text.join(delim !== undefined ? delim : ", ");
+};
+
+
+
+// ------------------------------------------------------------------------
+//  _   _ _____ _____ ____  
+// | | | |_   _|_   _|  _ \ 
+// | |_| | | |   | | | |_) |
+// |  _  | | |   | | |  __/ 
+// |_| |_| |_|   |_| |_|    
+//                          
+// ------------------------------------------------------------------------
+PME.Util.HTTP = {};
+
+function hostNameForURL(url) {
+	return (/^(https?:\/\/[^\/]+)\//.exec(pageURL)[1] || "").toLowerCase();
+}
+
+function httpRequest(reqURL, callback) {
+	var pageHost = hostNameForURL(pageURL),
+		reqHost = hostNameForURL(reqURL);
+
+	if (! reqHost.length)
+		reqHost = pageHost;
+
+	if (window.XDomainRequest && pageHost != reqHost)
+}
+
+PME.Util.HTTP.doGet = function(url, callback, charset) {
+	log("HTTP request: ", url);
+	var xhr = new XMLHttpRequest();
+
+	xhr.addEventListener("load", function() {
+		log("HTTP response: ", xhr, xhr.status)
+		callback(xhr.responseText);
+	}, false);
+	xhr.addEventListener("error", function() {
+		log("HTTP error: ", xhr);
+		callback("");
+	}, false);
+	xhr.addEventListener("abort", function() {
+		log("HTTP canceled: ", xhr);
+		callback("");
+	}, false);
+
+	try {
+		xhr.open("GET", url, true);
+		xhr.send();
+	}
+	catch(e) {
+		log("error during XHR operation", e);
+		setTimeout(function() { callback(""); }, 1);
+	}
+};
+
+PME.Util.HTTP.doPost = function(url, data, callback, headers, charset) {
+	setTimeout(function() {
+		callback("");	// simulate empty response
+	}, 1);
+};
+
+
+
+// ------------------------------------------------------------------------
+//   __ _ _ _                
+//  / _(_) | |_ ___ _ __ ___ 
+// | |_| | | __/ _ \ '__/ __|
+// |  _| | | ||  __/ |  \__ \
+// |_| |_|_|\__\___|_|  |___/
+//                           
+// ------------------------------------------------------------------------
+function ValueFilter() {
+	var filters = [], st;
+
+	function addFilter(fn) {
+		filters.push(fn);
+		return st;
+	}
+
+	function _applyFilters(vals, context) {
+		for (var fi=0; fi<filters.length; ++fi) {
+			vals = purge(flatten(vals));
+
+			for (var vi=0; vi<vals.length; ++vi) {
+				try {
+					vals[vi] = filters[fi](vals[vi], context);
+				}
+				catch(ex) {	}
+			}
+
+			vals = purge(vals);
+		}
+
+		return flatten(vals);
+	}
+
+	st = {
+		addFilter: addFilter,
+		_applyFilters: _applyFilters,
+
+		trim: function() { return addFilter(
+			function(str) {
+				return str.replace(/^\s+|\s+$/g, '');
+			}
+		);},
+		trimInternal: function() { return addFilter(
+			function(str) {
+				return str.replace(/\s+/g, ' ');
+			}
+		);},
+		replace: function(find, subst) { return addFilter(
+			function(str) {
+				return str.replace(find, subst);
+			}
+		);},
+		unescape: function() { return addFilter(
+			function(str) {
+				return unescape(str);
+			}
+		);},
+		unescapeHTML: function() { return addFilter(
+			function(str) {
+				return str; // TBI
+			}
+		);},
+		cleanAuthor: function(type, isReversed) { return addFilter(
+			function(str) {
+				return str; // TBI
+			}
+		);},
+		match: function(against, matchIndex) { return addFilter(
+			function(str) {
+				var m = str.match(against);
+				if (m)
+					m = m[matchIndex || 0];
+				return m;
+			}
+		);},
+		prepend: function(prefix) { return addFilter(
+			function(str) {
+				return prefix + str;
+			}
+		);},
+		append: function(suffix) { return addFilter(
+			function(str) {
+				return str + suffix;
+			}
+		);},
+		key: function(keyName) { return addFilter(
+			function(obj) {
+				return obj[keyName];
+			}
+		);},
+		split: function(sep) { return addFilter(
+			function(str) {
+				return str.split(sep);
+			}
+		);},
+		capitalizeTitle: function() { return addFilter(
+			function(str) {
+				return str; // TBI
+			}
+		);}
+	};
+	return st;
+}
+
+function PageText() {
+	var pt = ValueFilter();
+
+	pt.evaluate = function(doc, url) {
+		var vals = url._applyFilters([doc.documentElement.innerHTML], doc);
+		return vals.length ? vals : false;
+	};
+
+	return pt;
+}
+
+function Url() {
+	var lk = ValueFilter();
+
+	lk.evaluate = function(doc, url) {
+		var vals = lk._applyFilters([url], doc);
+		return vals.length ? vals : false;
+	}
+
+	return lk;
+}
+
+function Xpath(selector) {
+	var xp = ValueFilter();
+	xp.text = function() { return xp.addFilter(
+		function(obj) {
+			return obj.textContent || obj;
+		}
+	);};
+
+	xp.evaluate = function(doc, url) {
+		var res  = doc.evaluate(selector, doc, null, XPathResult.ANY_TYPE, null),
+			type = res.resultType,
+			vals = [],
+			v;
+
+		if (type == XPathResult.STRING_TYPE)
+			vals.push(res.stringValue);
+		else {
+			if (type == XPathResult.ORDERED_NODE_ITERATOR_TYPE || type == XPathResult.UNORDERED_NODE_ITERATOR_TYPE) {
+				while(v = res.iterateNext())
+					vals.push(v);
+			}
+		}
+
+		vals = xp._applyFilters(vals, doc);
+		return vals.length ? vals : false;
+	};
+
+	return xp;
+}
+
+
+// ------------------------------------------------------------------------
+//                                         
+//  ___  ___ _ __ __ _ _ __   ___ _ __ ___ 
+// / __|/ __| '__/ _` | '_ \ / _ \ '__/ __|
+// \__ \ (__| | | (_| | |_) |  __/ |  \__ \
+// |___/\___|_|  \__,_| .__/ \___|_|  |___/
+//                    |_|                  
+// ------------------------------------------------------------------------
+window.FW = (function(){
+	var scrapers = [];
+
+	function ScraperBase(spec) {
+		function evalItem(item, doc, url) {
+			if(typeof item == "object") {
+				if(item instanceof Array) {
+					return flatten(
+						map(item, function(subItem) {
+							return evalItem(subItem, doc, url);
+						})
+					);
+				}
+				return item.evaluate(doc, url);
+			}
+
+			if (typeof item == "function")
+				return item(doc, url);
+
+			return item;
+		}
+
+		return { spec: spec, evalItem: evalItem };
+	}
+
+	function Scraper(spec) {
+		var scrp = ScraperBase(spec);
+
+		scrp.run = function(doc, url, itemCallback, completionCallback) {
+			var skipList  = { detect:1, itemType:1, attachments:1 },
+				multiList = { creators: 1, tags: 1 },
+
+				item = map(spec, function(val, key) {
+					if (key in skipList)
+						return undefined;
+
+					var procVal = scrp.evalItem(val, doc, url);
+					// log("Scraper got kv", key, procVal);
+
+					if (key in multiList)
+						return flatten([procVal]);
+
+					return (procVal instanceof Array) ? procVal[0] : procVal;
+				});
+
+			item.itemType = spec.itemType;
+			itemCallback(item);
+			completionCallback();
+		};
+
+		scrapers.push(scrp);
+		return scrp;
+	}
+
+	function MultiScraper(spec) {
+		var scrp = ScraperBase(spec);
+
+		scrp.run = function(doc, url, itemCallback, completionCallback) {
+			// TBI
+			completionCallback();
+		};
+
+		scrapers.push(scrp);
+		return scrp;
+	}
+
+	function detectWeb(doc, url) {
+		var eligible = filter(scrapers, function(sc) {
+			return !!sc.evalItem(sc.spec.detect, doc, url);
+		});
+
+		log("FW.detectWeb eligible scrapers", eligible);
+
+		return eligible.length > 0;
+	}
+
+	function doWeb(doc, url) {
+		log("FW.doWeb called");
+
+		var scraper = filter(scrapers, function(sc) {
+			return !!sc.evalItem(sc.spec.detect, doc, url);
+		})[0];
+
+		log("FW.doWeb using scraper", scraper);
+
+		scraper.run(doc, url,
+			function itemDone(item) {
+				PME.items.push(item);
+			},
+			function allDone() {
+				PME.done();
+			}
+		);
+	}
+
+
+	return {
+		Scraper: Scraper,
+		MultiScraper: MultiScraper,
+		PageText: PageText,
+		Xpath: Xpath,
+		Url: Url,
+		
+		detectWeb: detectWeb,
+		doWeb: doWeb
+	};
+}());
+
+
+
+// ------------------------------------------------------------------------
+//            _                  _                 
+//   _____  _| |_ _ __ __ _  ___| |_ ___  _ __ ___ 
+//  / _ \ \/ / __| '__/ _` |/ __| __/ _ \| '__/ __|
+// |  __/>  <| |_| | | (_| | (__| || (_) | |  \__ \
+//  \___/_/\_\\__|_|  \__,_|\___|\__\___/|_|  |___/
+//                                                 
+// ------------------------------------------------------------------------
+function loadExtractor(name) {
+	log("loading extractor " + name);
+	var scr = document.createElement("script");
+	scr.src = extractorsBaseURL + name + ".js";
+	transScript = scr;
+	scr.onerror = function() {
+		log("ERROR: extractor failed to load: ", name);
+		completed(null);
+	}
+	document.getElementsByTagName("head")[0].appendChild(scr);
+}
+
+function extractorLoaded(spec, api) {
+	log("extractor loaded ", spec.label, spec);
+	try {
+		var detect = api.detectWeb(pageDoc, pageURL)
+		log("detectWeb returned ", detect);
+		if (detect)
+			api.doWeb(pageDoc, pageURL);
+		else
+			completed(null);
+	}
+	catch(e) {
+		log("ERROR during extraction", e, e.message);
+		completed(null);
+	}
+}
+
+function exporterNameForURL(url) {
+	var name = null;
+
+	for (var re in urlMatchers) {
+		if (new RegExp(re).test(url)) {
+			name = urlMatchers[re];
+			break;
+		}
+	}
+
+	return name;
+}
+
+
+// ------------------------------------------------------------------------
+//                   _   _                
+//  _ __ _   _ _ __ | |_(_)_ __ ___   ___ 
+// | '__| | | | '_ \| __| | '_ ` _ \ / _ \
+// | |  | |_| | | | | |_| | | | | | |  __/
+// |_|   \__,_|_| |_|\__|_|_| |_| |_|\___|
+//                                        
+// ------------------------------------------------------------------------
+function vanish() {
+return;
+	window.PME = undefined;
+	window.FW = undefined;
+
+	try {
+		if (transScript)
+			transScript.parentNode.removeChild(transScript);
+		if (window.PME_SCR)
+			PME_SCR.parentNode.removeChild(PME_SCR);
+	} catch(e) {}
+
+	window.PME_SCR = undefined;
+	window.PME_SRV = undefined;
+}
+
+function normalizeData(data) {
+	return data;
+}
+
+function completed(data) {
+	if (data)
+		data = normalizeData(data);
+	log("completed, data = ", data);
+
+	pmeCallback && pmeCallback(data);
+	setTimeout(vanish, 1);
+}
+
+function getPageMetaData(callback) {
+	log("getPageMetdaData start");
+	try {
+		// main accesspoint
+		pageURL = document.location.href;
+		pageDoc = document;
+		pmeCallback = callback;
+
+		var expName = exporterNameForURL(pageURL);
+
+		if (! expName)
+			completed(null);
+		else
+			loadExtractor(expName);
+	}
+	catch(e) {
+		log("ERROR during initialisation", e, e.message);
+		completed(null);
+	}
+}
+
+// -- expose extractor APIs
+PME.loadExtractor = loadExtractor;
+PME.extractorLoaded = extractorLoaded;
+PME.getPageMetaData = getPageMetaData;
+
+}());
---
+++ extractors/EBSCOhost.js
@@ -0,0 +1,320 @@
+(function(){
+var translatorSpec =
+{
+	"translatorID": "d0b1914a-11f1-4dd7-8557-b32fe8a3dd47",
+	"label": "EBSCOhost",
+	"creator": "Simon Kornblith, Michael Berkowitz, Josh Geller",
+	"target": "^https?://[^/]+/(?:eds|bsi|ehost)/(?:results|detail|folder)",
+	"minVersion": "2.1",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsib",
+	"lastUpdated": "2012-07-11 13:49:31"
+}
+
+function detectWeb(doc, url) {
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? function(prefix) {
+		if (prefix == 'x') { return namespace; } else { return null; }
+	} : null;
+
+	// See if this is a search results or folder results page
+	var searchResult = doc.evaluate('//ul[@class="result-list" or @class="folder-list"]/li/div[@class="result-list-record" or @class="folder-item"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
+	if(searchResult) {
+		return "multiple";
+	}
+
+	var xpath = '//a[@class="permalink-link"]';
+	var persistentLink = doc.evaluate(xpath, doc, nsResolver, XPathResult.ANY_TYPE, null);
+	if(persistentLink) {
+		return "journalArticle";
+	}
+}
+
+/*
+ * given the text of the delivery page, downloads an item
+ */
+function downloadFunction(text, url) {
+		var an = url.match(/_(\d+)_AN/);
+		var pdf = false;
+		var risDate = false;
+		var queryString = {};
+		url.replace(
+			new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+				function($0, $1, $2, $3) { queryString[$1] = $3; }
+		);
+		pdf = "/ehost/pdfviewer/pdfviewer?sid="+queryString["sid"]+"&vid="+queryString["vid"];
+
+		if (text.match(/^Y1\s+-(.*)$/m)) {
+			risDate = text.match(/^Y1\s+-(.*)$/m);
+		}
+
+		if (!text.match(/^TY\s\s-/m)) { text = text+"\nTY  - JOUR\n"; }
+		// load translator for RIS
+		var translator = Zotero.loadTranslator("import");
+		translator.setTranslator("32d59d2d-b65a-4da4-b0a3-bdd3cfb979e7");
+		translator.setString(text);
+		//Zotero.debug(text);
+		translator.setHandler("itemDone", function(obj, item) {
+			if (text.match(/^L3\s+-\s*(.*)/m)) {
+				item.DOI = text.match(/^L3\s+\-\s*(.*)/m)[1];
+			}
+			if (text.match(/^M3\s+-\s*(.*)/m)) {
+				if (item.DOI == text.match(/^M3\s+\-\s*(.*)/m)[1]) { item.DOI = ""; }
+			}
+			
+			// There are cases where the RIS type isn't good--
+			// there is sometimes better data in M3
+			// This list should be augmented over time
+			if (item.itemType == "document") {
+				var m3 = text.match(/^M3\s+-\s*(.*)$/m);
+				if (m3 && m3[1] == "Literary Criticism")
+					item.itemType = "journalArticle";
+			}
+			
+			if (text.match(/^DO\s+-\s*(.*)/m)) {
+				item.DOI = text.match(/^DO\s+-\s*(.*)/m)[1];
+			}
+			if (text.match(/^T1\s+-/m)) {
+				item.title = text.match(/^T1\s+-\s*(.*)/m)[1];
+			}
+
+			// Strip final period from title if present
+			item.title = item.title.replace(/\.$/,'');
+			// Get the accession number from URL or elsewhere
+			if (an) {
+				an = an[1];
+				item.callNumber = an;
+					
+			} else {
+				an = item.url.match(/AN=([0-9]+)/);
+				if (an) an = an[1];
+			}
+
+			if (risDate) {
+				var year = risDate[1].match(/\d{4}/);
+				var extra = risDate[1].match(/\/([^\/]+)$/);
+				// If we have a double year in risDate, use last section
+				if (year && extra && extra[1].indexOf(year[0]) !== -1) {
+					item.date = extra[1];
+				}
+				// Frequently have dates like "Spring2009";
+				// need to insert space to keep Zotero happy
+				item.date = item.date.replace(/([a-z])([0-9]{4})$/,"$1 $2");
+			}
+
+			// RIS translator tries to download the link in "UR"
+			item.attachments = [];
+
+			// But keep the stable link as a link attachment
+			if(item.url) {
+				// Trim the ⟨=cs suffix -- EBSCO can't find the record with it!
+				item.url = item.url.replace(/(AN=[0-9]+)⟨=[a-z]{2}/,"$1").replace(/#.*$/,'');
+				item.attachments.push({url: item.url+"&scope=cite",
+							title: "EBSCO Record",
+							mimeType: "text/html",
+							snapshot: false});
+				item.url = "";
+			}
+	
+			// A lot of extra info is jammed into notes by the RIS translator
+			item.notes = [];
+		
+		//not all items have a permalink if they don't we use the temporary URL
+		if (item.url) var stablelink= item.url;
+		else stablelink = url;
+		
+		// Since order of requests might matter, let's grab the stable link, then the PDF
+		Zotero.Utilities.doGet(stablelink, function (doc) { Zotero.Utilities.doGet(pdf, function (text) {
+				var realpdf = text.match(/<embed id="pdfEmbed"[^>]*>/);
+				if(realpdf) {
+					realpdf = text.match(/<embed[^>]*src="([^"]+)"/);
+					if (realpdf) {
+						realpdf = realpdf[1].replace(/&amp;/g, "&").replace(/#.*$/,'')
+								.replace(/K=\d+/,"K="+an);
+						//Zotero.debug("PDF for "+item.title+": "+realpdf);
+						item.attachments.push({url:realpdf,
+								title: "EBSCO Full Text",
+								mimeType:"application/pdf"});
+					}
+				}
+			}, function () { item.complete(); }); }, function () { return true; });
+		//	item.complete()
+		}); 
+		
+		
+		translator.translate();
+}
+
+var host;
+
+function doWeb(doc, url) {
+	var namespace = doc.documentElement.namespaceURI;
+	var nsResolver = namespace ? function(prefix) {
+		if (prefix == 'x') { return namespace; } else { return null; }
+	} : null;
+
+	var hostRe = new RegExp("^(https?://[^/]+)/");
+	var hostMatch = hostRe.exec(url);
+	host = hostMatch[1];
+
+	var searchResult = doc.evaluate('//ul[@class="result-list" or @class="folder-list"]/li/div[@class="result-list-record" or @class="folder-item"]', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext();
+
+	if(searchResult) {
+		/* Get title links and text */
+		var titlex = '//a[@class = "title-link color-p4"]';
+		var titles = doc.evaluate(titlex, doc, nsResolver, XPathResult.ANY_TYPE, null);
+
+		/* Get folder data for AN, DB, and tag */
+		var folderx = '//span[@class = "item add-to-folder"]/input/@value';
+		var folderData = doc.evaluate(folderx, doc, nsResolver, XPathResult.ANY_TYPE, null);
+
+		var items = {};
+		var folderInfos = {};
+		var title, folderInfo;
+
+		/* load up urls, title text and records keys (DB, AN, tag) */
+		while (title = titles.iterateNext()) {
+			items[title.href] = title.textContent;
+//Z.debug(items[title.href])
+			folderInfo = folderData.iterateNext();
+			folderInfos[title.href] = folderInfo.textContent;
+			//Z.debug(folderInfos[title.href])
+		}
+
+		Zotero.selectItems(items, function (items) {
+				if(!items) {
+					return true;
+				}
+
+				/* Get each citation page and pass in record key (db, tag, an) since data does not exist in an easily digestable way on this page */
+				var urls = [];
+				var infos = [];
+				var i;
+				for(i in items) {
+					urls.push(i);
+					infos.push(folderInfos[i]);
+				}
+
+				var run = function(urls, infos) {
+					var url, info;
+					if (urls.length == 0 || infos.length == 0) {
+						Zotero.done();
+						return true;
+					}
+					url = urls.shift();
+					info = infos.shift();
+					Zotero.Utilities.processDocuments(url.replace(/#.*$/,''),
+						function (newDoc) { doDelivery(doc, nsResolver, info, function () { run(urls, infos) }); },
+						function () { return true; });
+				};
+
+				run(urls, infos);
+
+				Zotero.wait();
+		});
+	} else {
+		/* Individual record. Record key exists in attribute for add to folder link in DOM */
+		doDelivery(doc, nsResolver, null, function () { Zotero.done(); return true; });
+		Zotero.wait();
+	}
+}
+function doDelivery(doc, nsResolver, folderData, onDone) {
+	if(folderData === null)	{
+		/* Get the db, AN, and tag from ep.clientData instead */
+		var script;
+		var scripts = doc.evaluate('//script[@type="text/javascript"]', doc, nsResolver, XPathResult.ANY_TYPE, null);
+		while (script = scripts.iterateNext().textContent) {
+			var clientData = script.match(/var ep\s*=\s*({[^;]*});/);
+			if (clientData) break;
+		}
+		if (!clientData) {return false;}
+			/* We now have the script containing ep.clientData */
+
+		/* The JSON is technically invalid, since it doesn't quote the
+		   attribute names-- we pull out the valid bit inside it. */
+		var clientData = script.match(/var ep\s*=\s*({[^;]*});/);
+		if (!clientData) { return false; }
+		clientData = clientData[1].match(/"currentRecord"\s*:\s*({[^}]*})/);
+		/* If this starts throwing exceptions, we should probably start try-elsing it */
+		folderData = JSON.parse(clientData[1]);
+	} else {
+		/* Ditto for this. */
+		// The attributes are a little different
+		folderData = JSON.parse(folderData);
+		folderData.Db = folderData.db;
+		folderData.Term = folderData.uiTerm;
+		folderData.Tag = folderData.uiTag;
+	}
+
+	var postURL = doc.evaluate('//form[@id="aspnetForm"]/@action', doc, nsResolver, XPathResult.ANY_TYPE, null).iterateNext().textContent;
+
+	var queryString = {};
+	postURL.replace(
+		new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+			function($0, $1, $2, $3) { queryString[$1] = $3; }
+	);
+
+	/* ExportFormat = 1 for RIS file */
+	postURL = host+"/ehost/delivery/ExportPanelSave/"+folderData.Db+"_"+folderData.Term+"_"+folderData.Tag+"?sid="+queryString["sid"]+"&vid="+queryString["vid"]+"&bdata="+queryString["bdata"]+"&theExportFormat=1";
+	Zotero.Utilities.HTTP.doGet(postURL, function (text) { downloadFunction(text, postURL); }, onDone);
+}
+
+/** BEGIN TEST CASES **/
+var testCases = [
+	{
+		"type": "web",
+		"url": "http://web.ebscohost.com/ehost/detail?sid=3f8c768f-e9bd-4c06-874e-30075b5a2e43%40sessionmgr15&vid=1&hid=11&bdata=JnNpdGU9ZWhvc3QtbGl2ZQ%3d%3d#db=a9h&AN=9606204477",
+		"items": [
+			{
+				"itemType": "journalArticle",
+				"creators": [
+					{
+						"lastName": "Brodsky",
+						"firstName": "Joseph",
+						"creatorType": "author"
+					}
+				],
+				"notes": [],
+				"tags": [
+					"POETS, Polish",
+					"HERBERT, Zbigniew, 1924-1998"
+				],
+				"seeAlso": [],
+				"attachments": [
+					{
+						"url": "http://search.ebscohost.com/login.aspx?direct=true&db=a9h&AN=9606204477&site=ehost-live&scope=cite",
+						"title": "EBSCO Record",
+						"mimeType": "text/html",
+						"snapshot": false
+					},
+					{
+						"url": "http://content.ebscohost.com/ContentServer.asp?T=P&P=AN&K=9606204477&S=R&D=a9h&EbscoContent=dGJyMMvl7ESeqa84zdnyOLCmr0meprFSs664SLaWxWXS&ContentCustomer=dGJyMPGstk%2B0qbJPuePfgeyx44Dt6fIA",
+						"title": "EBSCO Full Text",
+						"mimeType": "application/pdf"
+					}
+				],
+				"title": "Zbigniew Herbert",
+				"publicationTitle": "Wilson Quarterly",
+				"date": "Winter93 1993",
+				"volume": "17",
+				"issue": "1",
+				"pages": "112",
+				"publisher": "Woodrow Wilson International Center for Scholars",
+				"ISBN": "03633276",
+				"ISSN": "03633276",
+				"abstractNote": "Introduces the poetry of Polish poet Zbigniew Herbert. Impression of difficulty in modern poetry; Polish poet Czeslaw Milosz; Herbert's 1980 Nobel Prize; Translations into English; Use of vers libre; Sample poems.",
+				"callNumber": "9606204477",
+				"libraryCatalog": "EBSCOhost",
+				"checkFields": "title"
+			}
+		]
+	}
+]
+/** END TEST CASES **/
+
+// Generated code, or at least, this will be generated:
+PME.extractorLoaded(translatorSpec, { detectWeb: detectWeb, doWeb: doWeb });
+}());
