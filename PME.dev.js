892315a84ac0ac882ab076513a4e6890a86a49f2
atrout <a.sigrid.trout@gmail.com>
2013-01-15 13:53:54 -0500

    removed log statement

--- PME.dev.js
+++ PME.dev.js
@@ -679,7 +679,6 @@ PME.Translator = function(type) {
 		waitForTranslatorClass(function() {
 			try {
 				log('run translator', trClass.name, trClass, 'with url', url, 'and doc', doc);
-				log("type: " + type);
 				if (type == "import")
 					trClass.api.doImport();
 				else if (type == "web")
