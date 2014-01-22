5ca06f3043ac2d687740146a0c65e96fb47f6cce
sircmyst <Caistarrin.Mystical@proquest.com>
2014-01-22 08:33:56 -0800

    Revert "hardcode test"
    This reverts commit ba91808a16e80d5a0f67b4c9c71109d71bd49d30.

--- extractors/Springer Link.js
+++ extractors/Springer Link.js
@@ -76,8 +76,7 @@
 
 				if (PDFlink != null && PDFlink != '')
 					item.attachments.push({
-						//url: window.location.protocol + "//" + window.location.host + PME.Util.getNodeText(PDFlink[0]),
-						url: "http://download.springer.com/static/pdf/759/art%253A10.1007%252Fs00146-013-0532-5.pdf?auth66=1390578364_f369a9c66a28d6c1af648763c53759de&ext=.pdf",
+						url: window.location.protocol + "//" + window.location.host + PME.Util.getNodeText(PDFlink[0]),
 						title: "Springer Link Full Text",
 						mimeType: "application/pdf"
 					});
