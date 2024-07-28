634690991c4bf1e8da88d33fbcdb30f6c2dccec3
Ohad Kimhy <ohad.kimhy@exlibrisgroup.com>
2024-07-28 11:34:28 +0300

    RW-9460 release changes

--- PME.js
+++ PME.js
@@ -40,7 +40,7 @@
 	iframe.style.borderStyle = "none";
 	iframe.setAttribute("frameborder", "0");
 	var scriptLocations = 'window.PME_SERVICE_PROVIDER="'+window.PME_SERVICE_PROVIDER+'";window.EXT_SERVICE_PROVIDER="'+window.EXT_SERVICE_PROVIDER+'";';
-	iframe.src = 'javascript:(function(){document.open();try{window.parent.document;}catch(e){document.domain="' + document.domain.replace(/[\\\"]/g, "\\$0")+'";}document.write(\'<!DOCTYPE html><html><head><script>'+scriptLocations+'</script><script src="'+common+'"></script><script src="'+inject+'"></script><script type="text/javascript" src="https://pme.proquest.com/archive_2024-04-18/masterlist.js"></script></head><body></body></html>\');document.close();})()';
+	iframe.src = 'javascript:(function(){document.open();try{window.parent.document;}catch(e){document.domain="' + document.domain.replace(/[\\\"]/g, "\\$0")+'";}document.write(\'<!DOCTYPE html><html><head><script>'+scriptLocations+'</script><script src="'+common+'"></script><script src="'+inject+'"></script><script type="text/javascript" src="https://pme.proquest.com/masterlist.js"></script></head><body></body></html>\');document.close();})()';
 	tag.appendChild(iframe);
 
 }());
